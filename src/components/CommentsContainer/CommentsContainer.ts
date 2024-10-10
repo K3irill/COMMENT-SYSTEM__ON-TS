29d76e618dfa1a8b604cf0fbbe0c3e22d4815b7a
Kirill <razdor.2019@mail.ru>
2024-10-10 17:48:47 +0300

    reply-system on comment

--- src/components/CommentsContainer/CommentsContainer.ts
+++ src/components/CommentsContainer/CommentsContainer.ts
@@ -31,7 +31,7 @@ export class CommentsContainer {
 
 		this.commentsFormContainer = document.createElement('div')
 		this.commentsFormContainer.classList.add('comments__form-container')
-		//-----------
+
 		const userDiv = document.createElement('div')
 		userDiv.classList.add('comments__user')
 		const userImage = document.createElement('img')
@@ -47,10 +47,9 @@ export class CommentsContainer {
 		usernameH2.textContent = 'Вы'
 		const maxLengthP = document.createElement('p')
 		maxLengthP.textContent = 'Макс. 1000 символов'
-		infoDiv.appendChild(usernameH2)
-		infoDiv.appendChild(maxLengthP)
+		infoDiv.append(usernameH2)
+		infoDiv.append(maxLengthP)
 
-		//-----------
 		const form = document.createElement('form')
 		form.id = 'comment-form'
 
@@ -61,56 +60,160 @@ export class CommentsContainer {
 		this.commentInput.maxLength = 1000
 
 		this.commentButton = document.createElement('input')
-		this.commentButton.type = 'button'
+		this.commentButton.type = 'submit'
 		this.commentButton.id = 'comment-btn'
 		this.commentButton.value = 'Отправить'
 
-		//-----------
-		form.appendChild(this.commentInput)
-		form.appendChild(this.commentButton)
+		form.append(this.commentInput)
+		form.append(this.commentButton)
 
-		//-----------
-		mainDiv.appendChild(infoDiv)
-		mainDiv.appendChild(form)
-		this.commentsFormContainer.appendChild(userDiv)
-		this.commentsFormContainer.appendChild(mainDiv)
+		mainDiv.append(infoDiv)
+		mainDiv.append(form)
+		this.commentsFormContainer.append(userDiv)
+		this.commentsFormContainer.append(mainDiv)
 
-		//-----------
 		this.commentsContent = document.createElement('div')
 		this.commentsContent.classList.add('comments__content')
 
-		///-----------
 		this.commentsContainer.append(this.commentsActivities)
 		this.commentsContainer.append(this.commentsFormContainer)
 		this.commentsContainer.append(this.commentsContent)
 
-		//-----------
-		this.commentButton.addEventListener('click', event => {
-			const commentValue = this.commentInput?.value.trim()
+		form.addEventListener('submit', event => {
+			event.preventDefault()
+			const commentValue = this.commentInput!.value.trim()
 			if (commentValue) {
-				this.createComment(commentValue)
-				if (this.commentInput) {
-					this.commentInput.value = ''
-				}
+				this.createComment(commentValue, false)
+				this.commentInput!.value = ''
 			}
 		})
+
+		this.createComment(
+			`Наверное, самая большая ошибка создателей сериала была в том, что они поставили уж слишком много надежд на поддержку фанатов вселенной. Как оказалось на деле, большинство 'фанатов' с самой настоящей яростью и желчью стали уничтожать сериал, при этом объективности в отзывах самый минимум.`,
+			false
+		)
 	}
 
-	private createComment(value: string): void {
+	private createComment(
+		value: string,
+		isReply: boolean,
+		parentComment?: HTMLDivElement
+	): void {
+		const commentWrapperInfo = document.createElement('div')
+		commentWrapperInfo.classList.add('comments__wrapper-info')
+
 		const commentElement = document.createElement('div')
 		commentElement.classList.add('comments__comment')
-		commentElement.innerHTML = `
-            <div class='comments__user'>
-                <img src=${userInfo.userImg} alt=${userInfo.userImg}/>
-            </div>
-            <div class='comments__main'>
-                <div class='comments__info'>
-                    <h2>${userInfo.userName}</h2>
-                </div>
-                <p class='comments__text'>${value}</p>
-            </div>
-        `
-		this.commentsContent.append(commentElement)
+
+		if (isReply) {
+			commentElement.classList.add('comments__comment--reply')
+		}
+
+		const userDiv = document.createElement('div')
+		userDiv.classList.add('comments__user')
+
+		const userImg = document.createElement('img')
+		userImg.src = userInfo.userImg
+		userImg.alt = userInfo.userName
+		userDiv.append(userImg)
+
+		const mainDiv = document.createElement('div')
+		mainDiv.classList.add('comments__main')
+
+		const infoDiv = document.createElement('div')
+		infoDiv.classList.add('comments__info')
+
+		const userNameHeading = document.createElement('h2')
+		userNameHeading.textContent = userInfo.userName
+
+		infoDiv.append(userNameHeading)
+
+		const commentText = document.createElement('p')
+		commentText.classList.add('comments__text')
+		commentText.textContent = value
+
+		const commentActivities = document.createElement('div')
+		commentActivities.classList.add('comment__activities', 'comment-activities')
+
+		const replyButton = document.createElement('button')
+		replyButton.textContent = 'Ответить'
+
+		if (isReply) {
+			replyButton.style.display = 'none'
+		}
+
+		const favoriteButton = document.createElement('button')
+		favoriteButton.textContent = 'В избранное'
+
+		const ratingDiv = document.createElement('div')
+		ratingDiv.classList.add('comment-activities__count')
+
+		const decreaseButton = document.createElement('button')
+		decreaseButton.textContent = '-'
+
+		const ratingCount = document.createElement('p')
+		ratingCount.textContent = '10'
+
+		const increaseButton = document.createElement('button')
+		increaseButton.textContent = '+'
+
+		ratingDiv.append(decreaseButton)
+		ratingDiv.append(ratingCount)
+		ratingDiv.append(increaseButton)
+
+		commentActivities.append(replyButton)
+		commentActivities.append(favoriteButton)
+		commentActivities.append(ratingDiv)
+
+		mainDiv.append(infoDiv)
+		mainDiv.append(commentText)
+		mainDiv.append(commentActivities)
+
+		commentWrapperInfo.append(userDiv)
+		commentWrapperInfo.append(mainDiv)
+
+		commentElement.append(commentWrapperInfo)
+		if (parentComment) {
+			parentComment.appendChild(commentElement)
+		} else {
+			this.commentsContent.prepend(commentElement)
+		}
+
+		replyButton.addEventListener('click', () => {
+			let existingReplyForm = mainDiv.querySelector('#reply-form')
+
+			if (existingReplyForm) {
+				existingReplyForm.remove()
+			} else {
+				const replyForm = document.createElement('form')
+				replyForm.id = 'reply-form'
+
+				const replyInput = document.createElement('input')
+				replyInput.type = 'text'
+				replyInput.id = 'reply-input'
+				replyInput.placeholder = 'Ваш ответ на комментарий'
+				replyInput.maxLength = 1000
+
+				const replySubmitButton = document.createElement('input')
+				replySubmitButton.type = 'submit'
+				replySubmitButton.id = 'replyComment-btn'
+				replySubmitButton.value = 'Отправить'
+
+				replyForm.append(replyInput)
+				replyForm.append(replySubmitButton)
+
+				mainDiv.append(replyForm)
+
+				replyForm.addEventListener('submit', event => {
+					event.preventDefault()
+					const replyValue = replyInput.value.trim()
+					if (replyValue) {
+						this.createComment(replyValue, true, commentElement)
+						replyInput.value = ''
+					}
+				})
+			}
+		})
 	}
 
 	public getElement(): HTMLDivElement {
