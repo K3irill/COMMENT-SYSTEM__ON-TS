ad44cf35141c266860fe37128fdd066fd0a28976
Kirill <razdor.2019@mail.ru>
2024-10-10 13:16:03 +0300

    movie container

--- src/components/MovieContainer/movieContainer.ts
+++ src/components/MovieContainer/movieContainer.ts
@@ -1,16 +1,26 @@
 import { Card } from '../Card/card.ts'
-
+import './movieContainer.scss'
 export class MovieContainer {
 	private movieContainer: HTMLDivElement
 	private cardContainer: HTMLDivElement
+	private movieContent: HTMLDivElement
+	private descriptionEl: HTMLDivElement
 	constructor() {
 		this.movieContainer = document.createElement('div')
-		this.movieContainer.classList.add('movie-container')
+		this.movieContainer.classList.add('movie__container')
+
+		this.movieContent = document.createElement('div')
+		this.movieContent.classList.add('movie__content')
 
 		this.cardContainer = document.createElement('div')
-		this.cardContainer.classList.add('card-container')
+		this.cardContainer.classList.add('card__container')
 
-		this.movieContainer.append(this.cardContainer)
+		this.descriptionEl = document.createElement('div')
+		this.descriptionEl.classList.add('movie__description')
+
+		this.movieContent.append(this.cardContainer)
+		this.movieContent.append(this.descriptionEl)
+		this.movieContainer.append(this.movieContent)
 		this.generateCards()
 	}
 	private generateCards() {
